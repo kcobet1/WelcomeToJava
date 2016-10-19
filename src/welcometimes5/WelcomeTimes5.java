9c3da6a02a696efa3768281d4e661b37dbba0958
Kate Cobet <katecobet@gmail.com>
2016-10-19 13:39:42 -0500

    First Commit

---
+++ src/welcometimes5/WelcomeTimes5.java
@@ -0,0 +1,15 @@
+
+package welcometimes5;
+
+public class WelcomeTimes5 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+    
+}
